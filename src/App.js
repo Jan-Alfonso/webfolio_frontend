cfa13f73be0c5c2ed1b0b3a2382f9d75bba799e6
Jan Alfonso <janmerissalfonso@gmail.com>
2024-11-21 13:44:30 +0800

    Initial Loader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
+import { BrowserRouter } from "react-router-dom";
+import Loader from "./components/Loader/Loader";
 import Header from "./components/Header/Header";
 
 function App() {
+
+
+  const [screenLoading, setScreenLoading] = useState(false);
+
+  useEffect(() => {
+    setScreenLoading(true);
+    setTimeout(() => {
+      setScreenLoading(false);
+    }, 4000);
+  }, []);
+
+
   return (
     <>
-      <Header />
+      {screenLoading ? (
+        <Loader />
+      ) : (
+        <BrowserRouter>
+          <>
+            <Header />
+          </>
+        </BrowserRouter>
+      )}
     </>
   );
 }
